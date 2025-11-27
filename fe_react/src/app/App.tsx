3f2fd822ba5da3928ed46ad6ad6818a6cd49e8c9
DrGnNatsu <phphuong.career@gmail.com>
2025-11-27 09:10:11 +0700

    Fix the problem UI

--- fe_react/src/app/App.tsx
+++ fe_react/src/app/App.tsx
@@ -1,14 +1,24 @@
 import './App.css'
-import { Outlet } from 'react-router-dom';
+import {Outlet} from 'react-router-dom';
+import {Card, CardDescription, CardHeader, CardTitle} from '../components/ui/card'
 
 function App() {
 
-  return (
-    <>
-        <h1>Hello world</h1>
-        <Outlet />
-    </>
-  )
+    return (
+        <>
+            <h1 className="text-red-900 bg-white">Hello world</h1>
+            <Card className="w-full max-w-sm">
+                <CardHeader>
+                    <CardTitle>Login to your account</CardTitle>
+                    <CardDescription>
+                        Enter your email below to login to your account
+                    </CardDescription>
+
+                </CardHeader>
+            </Card>
+            <Outlet />
+        </>
+    )
 }
 
 export default App
