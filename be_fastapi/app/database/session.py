fd3f0d228c4134485fccbcf47deaee5088bc6e01
DrGnNatsu <phphuong.career@gmail.com>
2025-12-18 20:21:11 +0700

    Fix (alembic): alembic can not read .env path

--- be_fastapi/app/database/session.py
+++ be_fastapi/app/database/session.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 from app.core.config import settings
 
-engine = create_engine(settings.DATABASE_URL)
+engine = create_engine(settings.DATABASE_URL_APP)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
